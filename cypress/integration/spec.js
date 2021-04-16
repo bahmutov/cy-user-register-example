e7b54b5eee53d13a56743ff23292d3ea8eb3f431
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-16 16:49:35 -0400

    use common variable

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,17 +1,28 @@
 /// <reference types="cypress" />
 
-it('sets the random user from the fixture list', () => {
+// use a common variable to store the random user
+let testUser
+
+before(() => {
   cy.fixture('data.json').then(({ users }) => {
     // the index k will be from 0 to users.length - 1
     const k = Cypress._.random(users.length - 1)
     expect(k, 'random user index').to.be.within(0, users.length - 1)
-    const testUser = users[k]
+    testUser = users[k]
 
     // we need to send the entire database object
     cy.request('POST', '/reset', {
       users: [testUser],
     })
-    cy.visit('/')
-    cy.contains('#user', `${testUser.name.first} ${testUser.name.last}`)
   })
+})
+
+it('sets the random user from the fixture list', () => {
+  cy.visit('/')
+  const name = testUser.name
+  cy.contains('#user', `${name.first} ${name.last}`)
+})
+
+it('has the test user', function () {
+  expect(testUser).to.be.an('object')
 })
