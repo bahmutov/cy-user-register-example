64edafc2ef1a1a8832f49069b8f9e7c8e7e0d50c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-16 17:19:14 -0400

    use memo

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,34 +1,41 @@
 /// <reference types="cypress" />
 
+// local variable to remember the once generated user
+let testUser
+
 Cypress.Commands.add('pickTestUser', () => {
+  if (testUser) {
+    return cy.wrap(testUser)
+  }
+
   cy.fixture('data.json').then(({ users }) => {
     // the index k will be from 0 to users.length - 1
     const k = Cypress._.random(users.length - 1)
     expect(k, 'random user index').to.be.within(0, users.length - 1)
-    Cypress.env('testUser', users[k])
+    testUser = users[k]
+    // yield the generated test user object
+    cy.wrap(testUser)
   })
 })
 
-before(() => {
-  cy.pickTestUser()
-})
-
 beforeEach(() => {
-  const testUser = Cypress.env('testUser')
-  // we need to send the entire database object
-  cy.request('POST', '/reset', {
-    users: [testUser],
+  cy.pickTestUser().then((testUser) => {
+    // we need to send the entire database object
+    cy.request('POST', '/reset', {
+      users: [testUser],
+    })
   })
 })
 
 it('sets the random user from the fixture list', () => {
-  const testUser = Cypress.env('testUser')
-  cy.visit('/')
-  const name = testUser.name
-  cy.contains('#user', `${name.first} ${name.last}`)
+  cy.pickTestUser().then((testUser) => {
+    cy.visit('/')
+    const name = testUser.name
+    cy.contains('#user', `${name.first} ${name.last}`)
+  })
 })
 
 it('has the test user', () => {
-  const testUser = Cypress.env('testUser')
-  expect(testUser).to.be.an('object')
+  // use .should assertion on the yielded object
+  cy.pickTestUser().should('be.an', 'object')
 })
