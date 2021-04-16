06056ff3dd468073a58bd5c92546b2c88c355b61
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-16 17:43:24 -0400

    use import

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,19 +1,13 @@
 /// <reference types="cypress" />
 
-// local variable to remember the once generated user
-let testUser
+import { users } from '../fixtures/data.json'
+
+// note that Cypress._ is available outside of any test.
+// the index k will be from 0 to users.length - 1
+const k = Cypress._.random(users.length - 1)
+expect(k, 'random user index').to.be.within(0, users.length - 1)
+const testUser = users[k]
 
 Cypress.Commands.add('pickTestUser', () => {
-  if (testUser) {
-    return cy.wrap(testUser)
-  }
-
-  cy.fixture('data.json').then(({ users }) => {
-    // the index k will be from 0 to users.length - 1
-    const k = Cypress._.random(users.length - 1)
-    expect(k, 'random user index').to.be.within(0, users.length - 1)
-    testUser = users[k]
-    // yield the generated test user object
-    cy.wrap(testUser)
-  })
+  cy.wrap(testUser)
 })
