6f91fd67324ac4b7d119c0da3162cdcb395f93f6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-16 16:19:05 -0400

    pick random user from the fixture

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,17 @@
 /// <reference types="cypress" />
 
-it('loads the first user', () => {
-  cy.visit('/')
-  cy.contains('#user', 'Joe Smith')
+it('sets the random user from the fixture list', () => {
+  cy.fixture('data.json').then(({ users }) => {
+    // the index k will be from 0 to users.length - 1
+    const k = Cypress._.random(users.length - 1)
+    expect(k, 'random user index').to.be.within(0, users.length - 1)
+    const testUser = users[k]
+
+    // we need to send the entire database object
+    cy.request('POST', '/reset', {
+      users: [testUser],
+    })
+    cy.visit('/')
+    cy.contains('#user', `${testUser.name.first} ${testUser.name.last}`)
+  })
 })
