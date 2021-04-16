79011a14b852cc2efda69d7b98afc6521fa609cf
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-16 17:00:22 -0400

    use Cypress.env

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -5,12 +5,12 @@ before(() => {
     // the index k will be from 0 to users.length - 1
     const k = Cypress._.random(users.length - 1)
     expect(k, 'random user index').to.be.within(0, users.length - 1)
-    Cypress.config('testUser', users[k])
+    Cypress.env('testUser', users[k])
   })
 })
 
 beforeEach(() => {
-  const testUser = Cypress.config('testUser')
+  const testUser = Cypress.env('testUser')
   // we need to send the entire database object
   cy.request('POST', '/reset', {
     users: [testUser],
@@ -18,13 +18,13 @@ beforeEach(() => {
 })
 
 it('sets the random user from the fixture list', () => {
-  const testUser = Cypress.config('testUser')
+  const testUser = Cypress.env('testUser')
   cy.visit('/')
   const name = testUser.name
   cy.contains('#user', `${name.first} ${name.last}`)
 })
 
 it('has the test user', () => {
-  const testUser = Cypress.config('testUser')
+  const testUser = Cypress.env('testUser')
   expect(testUser).to.be.an('object')
 })
